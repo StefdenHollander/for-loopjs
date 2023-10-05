1567a80b1d0537a27d27f7d93da21edb7b8486e4
S den Hollander <302045198@student.rocmondriaan.nl>
2023-10-05 12:57:55 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,27 @@
+console.log("main.js loaded...")
+//opdracht 1
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log("Einde programma");
+//opdracht 2
+for (let i = 0; i < 21; i++) {
+    console.log(i);
+}
+console.log("Einde programma")
+//opdracht 3
+for (let i = 2; i < 21; i++) {
+    console.log(i);
+}
+console.log("Einde programma")
+//opdracht 4
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+console.log("Einde programma")
+//opdracht 5
+for (let i = 40; i >= 4; i--) {
+    console.log(i);
+}
+console.log("Einde programma")
+//opdracht 6
